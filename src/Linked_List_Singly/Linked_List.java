298cc67820041c282c757362d0ea99f8387a7658
dinhloc2510@gmail.com <dinhloc2510@gmail.com>
2024-11-08 15:30:40 +0700

    Linked List SingLy

--- src/Linked_List_Singly/Linked_List.java
+++ src/Linked_List_Singly/Linked_List.java
@@ -38,4 +38,59 @@ public class Linked_List<T> {
 			tmp.setNext(newNode);
 		}
 	}
+
+	public void insertAfter(T key, T toInsert) {
+
+		Node1<T> tmp = head;
+
+		while (tmp != null && tmp.getData().equals(key)) {
+			tmp = tmp.getNext();
+		}
+		if (tmp != null) {
+//			cách 1
+//			Node1<T> newNode1 = new Node1<>();
+//			newNode.setData(toInsert);
+//			newNode.setNext(tmp.getNext());
+//			cách 2
+			Node1<T> newNode = new Node1<>(toInsert, tmp.getNext());
+			tmp.setNext(newNode);
+		}
+	}
+
+	public boolean remove(T key) {
+		if (head == null)
+			return false;
+
+		if (head.getData().equals(key)) {
+			this.head = head.getNext();
+			return true;
+		}
+		Node1<T> prep = null;
+		Node1<T> cur = head;
+		while (cur != null && !cur.getData().equals(key)) {
+			prep = cur;
+			cur = cur.getNext();
+		}
+		if (cur == null)
+			return false;
+		prep.setNext(cur.getNext());
+		return true;
+
+	}
+
+	public boolean isEmpty() {
+		return head == null;
+	}
+
+	public int size() {
+		int count = 0;
+		Node1<T> tmp = head;
+		while (tmp != null) {
+			count++;
+			System.out.println(tmp.getData());
+			tmp = tmp.getNext();
+		}
+		return count;
+	}
+
 }
--- src/Linked_List_Singly/Test1.java
+++ src/Linked_List_Singly/Test1.java
@@ -10,5 +10,12 @@ public class Test1 {
 		System.out.println("Sau khi add");
 		ll.addLast("Nguyen Minh Manh");
 		ll.traverse();
+		System.out.println("Sau khi add tùy chọn");
+		ll.insertAfter("Nguyen Dinh Minh", "Nguyen Minh Manh");
+		ll.traverse();
+		System.out.println("Sau khi xoa");
+		ll.remove("Nguyen Dinh Loc");
+		ll.traverse();
+		System.out.println("Số phần tử là: " + ll.size());
 	}
 }
