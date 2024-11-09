82d6954f470e15eca631deb1dcb8eab6eaadbc7a
dinhloc2510@gmail.com <dinhloc2510@gmail.com>
2024-11-09 21:57:56 +0700

    Review Knowledge About Linked List Singly

---
+++ src/Linked_List_Singly_Review_Knowledge/Node.java
@@ -0,0 +1,32 @@
+package Linked_List_Singly_Review_Knowledge;
+
+public class Node<T> {
+	private T data;
+	private Node<T> next;
+
+	public Node() {
+
+	}
+
+	public T getData() {
+		return data;
+	}
+
+	public void setData(T data) {
+		this.data = data;
+	}
+
+	public Node<T> getNext() {
+		return next;
+	}
+
+	public void setNext(Node<T> next) {
+		this.next = next;
+	}
+
+	public Node(T data, Node<T> next) {
+		this.data = data;
+		this.next = next;
+	}
+
+}
