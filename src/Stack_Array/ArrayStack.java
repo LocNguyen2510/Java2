72cdf48d368849f9ac4ca63764bde42df9146784
dinhloc2510@gmail.com <dinhloc2510@gmail.com>
2024-11-17 21:36:03 +0700

    Array Stack

---
+++ src/Stack_Array/ArrayStack.java
@@ -0,0 +1,37 @@
+package Stack_Array;
+
+public class ArrayStack implements StackADT {
+	private int top;
+	private Object[] stack;
+
+	@Override
+	public void push(Object element) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Object pop() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object peek() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
