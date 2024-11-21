d249ab9ac7a276bc1a6b8acf00000fa033e1d071
dinhloc2510@gmail.com <dinhloc2510@gmail.com>
2024-11-21 18:59:43 +0700

    Build Stack With Linked List

---
+++ src/Stack_Linked_List/Node1.java
@@ -0,0 +1,31 @@
+package Stack_Linked_List;
+
+public class Node1<T> {
+	private T data;
+	private Node1<T> next;
+
+	public Node1() {
+
+	}
+
+	public T getData() {
+		return data;
+	}
+
+	public void setData(T data) {
+		this.data = data;
+	}
+
+	public Node1<T> getNext() {
+		return next;
+	}
+
+	public void setNext(Node1<T> next) {
+		this.next = next;
+	}
+
+	public Node1(T data, Node1<T> next) {
+		this.data = data;
+		this.next = next;
+	}
+}
---
+++ src/Stack_Linked_List/Test1.java
@@ -0,0 +1,7 @@
+package Stack_Linked_List;
+
+public class Test1 {
+	public static void main(String[] args) {
+	}
+
+}
