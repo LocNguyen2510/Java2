b75d7c4eb5008195ff86d0aa656d904d37f3a59c
dinhloc2510@gmail.com <dinhloc2510@gmail.com>
2024-11-23 17:46:23 +0700

    Stack Linked List

--- src/Stack_Linked_List/LinkedStack.java
+++ src/Stack_Linked_List/LinkedStack.java
@@ -1,5 +1,7 @@
 package Stack_Linked_List;
 
+import java.util.EmptyStackException;
+
 public class LinkedStack<T> implements StackADT<T> {
 	private int count;
 	private LinkedNode<T> top;
@@ -7,37 +9,42 @@ public class LinkedStack<T> implements StackADT<T> {
 	public LinkedStack() {
 		count = 0;
 		top = null;
-		count++;
+
 	}
 
 	@Override
 	public void push(T element) {
-		// TODO Auto-generated method stub
 		LinkedNode<T> temp = new LinkedNode<>(element, top);
 		top = temp;
+		count++;
 	}
 
 	@Override
-	public T pop() {
-		// TODO Auto-generated method stub
-		return null;
+	public T pop() throws EmptyStackException {
+		if (isEmpty()) {
+			throw new EmptyStackException();
+		}
+		T result = top.getData(); // lấy ra giá trị của top node
+		top = top.getNext();
+		count--;
+		return result;
 	}
 
 	@Override
-	public T peek() {
-		// TODO Auto-generated method stub
-		return null;
+	public T peek() throws EmptyStackException {
+		if (isEmpty()) {
+			throw new EmptyStackException();
+		}
+		return top.getData();
 	}
 
 	@Override
 	public boolean isEmpty() {
-		// TODO Auto-generated method stub
-		return false;
+		return count == 0;
 	}
 
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub
 		return count;
 	}
 
