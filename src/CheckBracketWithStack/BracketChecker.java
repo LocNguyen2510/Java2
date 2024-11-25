36dbcf0fafe40c4f133e32df592c21824f442c2c
dinhloc2510@gmail.com <dinhloc2510@gmail.com>
2024-11-25 17:48:31 +0700

    Check Braket with Stack

--- src/CheckBracketWithStack/BracketChecker.java
+++ src/CheckBracketWithStack/BracketChecker.java
@@ -6,8 +6,24 @@ public class BracketChecker {
 		// duyệt từng kí tự
 //		tình huống thứ 0: những kí tự khác => không quan tâm 
 //		tình huống thứ 1: Mở ngoặc [,{,(
+		for (int i = 0; i < code.length(); i++) {
+			char ch = code.charAt(i);
+			if (ch == '{' || ch == '[' || ch == '(') {
+				stack.push(ch);
+			} else if (ch == '}' || ch == ']' || ch == ')') {
+				if (stack.isEmpty()) {
+					return false;
+				}
+				char top = stack.pop();
+				if (ch == '}' && top != '{' || ch == ']' && top != '[' || ch == ')' && top != '(') {
+					return false;
+				}
+			}
+		}
+		return stack.isEmpty();
 //		tình huống thứ 2: Đóng ngoặc ], }, )
-
+//		Kiểm tra stack có rỗng hay không. nếu rỗng là sai 
+//		nếu không rỗng => lấy kí tự ở đỉnh stack =>  nếu kí tự ở đỉnh stack giống với kí tự hiện tại => true 
+//		còn ngược lại sẽ là false
 	}
-
 }
--- src/CheckBracketWithStack/Test1.java
+++ src/CheckBracketWithStack/Test1.java
@@ -2,19 +2,21 @@ package CheckBracketWithStack;
 
 public class Test1 {
 	public static void main(String[] args) {
-
-		LinkedStack<Integer> mystack = new LinkedStack<>();
-		mystack.push(13);
-		mystack.push(5);
-		mystack.push(18);
-		int x = mystack.pop();
-		System.out.println(x);
-		int y = mystack.peek();
-		System.out.println(y);
-		int z = mystack.pop();
-		System.out.println(z);
-		System.out.println("số phần tử trong mảng: " + mystack.size());
-
+		String code1 = "{[()]}";
+		String code2 = "{()]}";
+		String code3 = "{[()}";
+		String code4 = "\r\n" + "public class LinkedNode<T> {\r\n" + "	private T data;\r\n"
+				+ "	private LinkedNode<T> next;\r\n" + "\r\n" + "	public LinkedNode() {\r\n" + "\r\n" + "	}\r\n"
+				+ "\r\n" + "	public T getData() {\r\n" + "		return data;\r\n" + "	}\r\n" + "\r\n"
+				+ "	public void setData(T data) {\r\n" + "		this.data = data;\r\n" + "	}\r\n" + "\r\n"
+				+ "	public LinkedNode<T> getNext() {\r\n" + "		return next;\r\n" + "	}\r\n" + "\r\n"
+				+ "	public void setNext(LinkedNode<T> next) {\r\n" + "		this.next = next;\r\n" + "	}\r\n" + "\r\n"
+				+ "	public LinkedNode(T data, LinkedNode<T> next) {\r\n" + "		this.data = data;\r\n"
+				+ "		this.next = next;\r\n" + "	}\r\n" + "}";
+		System.out.println("Check code1: " + BracketChecker.isBalanced(code1));
+		System.out.println("Check code1: " + BracketChecker.isBalanced(code2));
+		System.out.println("Check code1: " + BracketChecker.isBalanced(code3));
+		System.out.println("Check code1: " + BracketChecker.isBalanced(code4));
 	}
 
 }
