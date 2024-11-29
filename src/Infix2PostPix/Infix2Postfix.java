79376ca928f94e996ee9939d0747429a0bb8a529
dinhloc2510@gmail.com <dinhloc2510@gmail.com>
2024-11-29 23:39:40 +0700

    Infix to Postfix

--- src/Infix2PostPix/Infix2Postfix.java
+++ src/Infix2PostPix/Infix2Postfix.java
@@ -4,6 +4,12 @@ public class Infix2Postfix {
 	private int precendence(char operator) {
 		switch (operator) {
 		case '+':
+		case '_':
+			return 1;
+		case '*':
+		case '/':
+			return 2;
+
 		}
 	}
 }
