4f5ea48f87d989abf12a40d693de81626e44857b
dinhloc2510@gmail.com <dinhloc2510@gmail.com>
2024-12-02 19:02:06 +0700

    Infix to Postfix with stack

--- src/Infix2PostPix/Infix2Postfix.java
+++ src/Infix2PostPix/Infix2Postfix.java
@@ -1,6 +1,9 @@
 package Infix2PostPix;
 
+import java.util.Stack;
+
 public class Infix2Postfix {
+//	hàm xác định độ ưu tiên của toán tử
 	private int precendence(char operator) {
 		switch (operator) {
 		case '+':
@@ -16,7 +19,13 @@ public class Infix2Postfix {
 		return -1;
 	}
 
+//hàm chuyển từ trung tố sang hậu tố
 	public static String infix2postfix(String expression) {
 		expression = expression.replace(' ', ' ');
+		String result = "";
+		Stack<Character> stack = new Stack<Character>();
+		for (int i = 0; i < expression.length(); i++) {
+			char c = expression.charAt(i);
+		}
 	}
 }
