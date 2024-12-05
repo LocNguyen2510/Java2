60de13e33e25dfe699c8dbf91d089aa72d68d4fb
dinhloc2510@gmail.com <dinhloc2510@gmail.com>
2024-12-05 11:39:17 +0700

    Review Knowledge about Infix to Postfix with Stack

--- src/InfixtoPostfix_Review_Knowledge/InfixtoPostfix.java
+++ src/InfixtoPostfix_Review_Knowledge/InfixtoPostfix.java
@@ -1,5 +1,34 @@
 package InfixtoPostfix_Review_Knowledge;
 
-public class InfixtoPostfix {
+import java.util.Stack;
 
+public class InfixtoPostfix {
+	private static int precendence(char operate) {
+		switch (operate) {
+		case '+':
+		case '-':
+			return 1;
+		case '*':
+		case '/':
+			return 2;
+		case '^':
+			return 3;
+
+		}
+		return -1;
+	}
+
+	public static String infixToPostfix(String exper) {
+		exper = exper.replace(" ", "");
+		String result = "";
+		Stack<Character> stack = new Stack<>();
+		boolean check = false;
+		for (int i = 0; i < exper.length(); i++) {
+			char c = exper.charAt(i);
+			if (!stack.isEmpty()) {
+
+			}
+		}
+
+	}
 }
