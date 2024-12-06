57fd1c4baff94362109bf8495a8b91f8e16a98c5
dinhloc2510@gmail.com <dinhloc2510@gmail.com>
2024-12-06 21:08:24 +0700

    Tính kết quả biểu thức toán học

--- src/Tinh_Ket_Qua_Bieu_Thuc_Toan_Hoc/Evaluator.java
+++ src/Tinh_Ket_Qua_Bieu_Thuc_Toan_Hoc/Evaluator.java
@@ -1,5 +1,11 @@
 package Tinh_Ket_Qua_Bieu_Thuc_Toan_Hoc;
 
+import java.util.Stack;
+
 public class Evaluator {
 //hàm tính giá trị biểu thức hậu tố
+	public static double evaluatePostfix(String expression) {
+		Stack<Double> stack = new Stack<>();
+		expression = expression.trim(); // loại bỏ khoảng trắng 2 dấu câu
+	}
 }
