066c5d91e0b45abea5957a97d2d76a463a701798
dinhloc2510@gmail.com <dinhloc2510@gmail.com>
2024-12-10 17:58:54 +0700

    Learn SQL with Java

--- src/Part2_Java_SQL/JavaSQL.java
+++ src/Part2_Java_SQL/JavaSQL.java
@@ -4,5 +4,7 @@ public class JavaSQL {
 
 	public static void main(String[] args) {
 		System.out.println("kết thúc quá trình học cấu trúc dữ liệu giải thuật với java");
+//		những công cụ: Eclipse, Xampp (MySQL), HeidiSQL
+		System.out.println("Ngày 1 sẽ ôn tập lại SQL");
 	}
 }
