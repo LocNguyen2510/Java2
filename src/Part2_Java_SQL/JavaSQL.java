deb0aacdd0bd51021bf5b76b90bc0fd4ab45b1e6
dinhloc2510@gmail.com <dinhloc2510@gmail.com>
2024-12-12 19:51:34 +0700

    learn SQL with Java

--- src/Part2_Java_SQL/JavaSQL.java
+++ src/Part2_Java_SQL/JavaSQL.java
@@ -6,5 +6,46 @@ public class JavaSQL {
 		System.out.println("kết thúc quá trình học cấu trúc dữ liệu giải thuật với java");
 //		những công cụ: Eclipse, Xampp (MySQL), HeidiSQL
 		System.out.println("Ngày 1 sẽ ôn tập lại SQL và dùng Xampp để kết nối sql");
+
+//		
+//		CREATE DATABASE ontap
+//		CHARACTER SET UTF8MB4
+//		COLLATE utf8mb4_unicode_ciontap;
+//		
+//		
+//		CREATE TABLE Persons(
+//				person_id INT NOT NULL AUTO_INCREMENT,
+//				last_name VARCHAR(20) NOT NULL,
+//				first_name VARCHAR(20) NOT NULL,
+//				gender VARCHAR(1),
+//				dob DATE,
+//				income DOUBLE,
+//				PRIMARY KEY(person_id));persons
+//				
+//				
+//				INSERT INTO persons(last_name,first_name,gender,dob,income)
+//				VALUES 
+//				("Vu","Nguyen Dinh","Male","2001-10-25",20000000),
+//				("Minh","Nguyen Van","Male","2000-5-18",15000000),
+//				("Toan","Pham Dinh","Female","2005-12-15",19000000)
+//
+//				 ;
+//				
+//				
+//				-- UPDATE persons AS p
+//				-- SET income =income+5000000;
+//				UPDATE persons
+//				SET income = income+200000
+//				WHERE last_name="Loc";
+//				
+//				
+//				DELETE FROM persons
+//				WHERE last_name<> "Loc";
+//
+//				
+//				
+//				SELECT * FROM persons;
+//				SELECT * FROM persons
+//				WHERE income >19000000;
 	}
 }
